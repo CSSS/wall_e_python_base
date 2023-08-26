80c86291b265931ca811bc5689dfff4acd2f5e5e
Jace Manshadi <jmanshad@protonmail.com>
2023-08-25 18:37:52 -0700

    adding --all flag so cache is also removed

--- build.sh
+++ build.sh
@@ -15,10 +15,11 @@ if [ -z "${DOCKER_HUB_USER_NAME}" ]; then
     exit 1
 fi
 
-docker system prune -f
 export wall_e_bottom_base_image_dockerfile="Dockerfile"
 
 docker image rm -f "${WALL_E_PYTHON_BASE_IMAGE}" || true
+docker system prune -f --all
+docker builder prune
 docker build --no-cache -t ${WALL_E_PYTHON_BASE_IMAGE} -f ${wall_e_bottom_base_image_dockerfile} .
 export WALL_E_BASE_ORIGIN_NAME="${WALL_E_PYTHON_BASE_IMAGE}"
 
@@ -28,4 +29,5 @@ docker push sfucsssorg/${WALL_E_PYTHON_BASE_IMAGE}
 
 docker rmi sfucsssorg/${WALL_E_PYTHON_BASE_IMAGE}
 docker image rm "${WALL_E_PYTHON_BASE_IMAGE}"
-docker system prune -f
+docker system prune -f --all
+docker builder prune
